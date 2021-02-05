c58cf4eee0ee4538e235e0b8b124bd0e3ba3df1e
Guillaume Bono <guillaume.bono@insa-lyon.fr>
2021-02-05 12:58:43 +0100

    Fixed register real2sim pos and export to jpeg!

--- script/register_sim2real_pos.py
+++ script/register_sim2real_pos.py
@@ -13,9 +13,10 @@ import quaternion
 
 
 IN_DIR = "out/real"
+OUT_DIR = "out/sim"
 CFG_PATH = "configs/locobot_pointnav_citi_sim.yaml"
-N_MEASURES = 5
-STEP_SIZE = 0.01
+N_MEASURES = 1
+STEP_SIZE = 0.05
 TURN_ANGLE = 1
 TILT_ANGLE = 1
 
@@ -34,12 +35,13 @@ def parse_args():
     parser = argparse.ArgumentParser(description=USAGE,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument("--input-dir", "-i", default=IN_DIR)
-    parser.add_argument("--output-dir", "-o", default=None)
+    parser.add_argument("--output-dir", "-o", default=OUT_DIR)
     parser.add_argument("--cfg-path", "-c", default=CFG_PATH)
     parser.add_argument("--n-measures", "-n", type=int, default=N_MEASURES)
     parser.add_argument("--step-size", type=float, default=STEP_SIZE)
     parser.add_argument("--turn-angle", type=float, default=TURN_ANGLE)
     parser.add_argument("--tilt-angle", type=float, default=TILT_ANGLE)
+    parser.add_argument("--no-grid", action="store_true")
     return parser.parse_args()
 
 
@@ -54,23 +56,43 @@ def setup_sim(step_size, turn_ang, tilt_ang):
 
 
 def stamped_images(input_dir):
-    img_path = os.path.join(input_dir, "x0.000_y0.000_z0.000_r0_rgb.jpeg")
-    yield (0, 0, 0, 0), cv2.imread(img_path)
-    for img_path in glob.iglob(os.path.join(input_dir, "*_rgb.jpeg")):
-        filename = os.path.basename(img_path)
-        x_real, y_real, z_real, a_real = (float(s[1:]) for s in filename.split('_')[:-1])
-        a_real = numpy.radians(a_real)
-        yield (x_real, y_real, z_real, a_real), cv2.imread(img_path)
+    filepath = os.path.join(input_dir, "x0.000_y0.000_z0.000_r0_rgb.jpeg")
+    yield (0, 0, 0), 0, cv2.imread(filepath), filepath
+    for filepath in glob.iglob(os.path.join(input_dir, "*_rgb.jpeg")):
+        filename = os.path.basename(filepath)
+        real_x, real_y, real_z, real_a = (float(s[1:]) for s in filename.split('_')[:-1])
+        real_a = numpy.radians(real_a)
+        yield [real_x, real_y, real_z], real_a, cv2.imread(filepath), filepath
 
 
-def print_pos(label, x, y, z, a, same_line=False):
+def draw_grid(img):
+    disp = img.copy()
+    H, W, _ = img.shape
+
+    cv2.line(disp, (W // 2, 0), (W // 2, H), (0, 0, 255))
+    cv2.line(disp, (0, H // 2), (W, H // 2), (0, 0, 255))
+
+    cv2.line(disp, (W // 3, 0), (W // 3, H), (0, 255, 0))
+    cv2.line(disp, (2 * W // 3, 0), (2 * W // 3, H), (0, 255, 0))
+    cv2.line(disp, (0, H // 3), (W, H // 3), (0, 255, 0))
+    cv2.line(disp, (0, 2 * H // 3), (W, 2 * H // 3), (0, 255, 0))
+
+    cv2.line(disp, (W // 6, 0), (W // 6, H), (255, 0, 0))
+    cv2.line(disp, (5 * W // 6, 0), (5 * W // 6, H), (255, 0, 0))
+    cv2.line(disp, (0, H // 6), (W, H // 6), (255, 0, 0))
+    cv2.line(disp, (0, 5 * H // 6), (W, 5 * H // 6), (255, 0, 0))
+
+    return disp
+
+
+def print_pos(label, pos, a, same_line=False):
     a_deg = numpy.degrees(a)
     pre="\r" if same_line else "\n"
-    print(f"{pre}{label: >12}: x={x:+7.3f} y={y:+7.3f} z={z:+7.3f} θ={a_deg:+4.0f}",
-          end="" if same_line else "\n")
+    print("{}{: >12}: x={:+7.3f} y={:+7.3f} z={:+7.3f} θ={:+4.0f}".format(
+        pre, label, *pos, a_deg), end="" if same_line else "\n")
 
 
-def step_sim(sim, key_code, sim_x, sim_y, sim_z, sim_a, sim_quat, step_size=STEP_SIZE):
+def step_sim(sim, key_code, sim_pos, sim_a, sim_quat, step_size=STEP_SIZE):
     obs = None
     reject = False
     if key_code == ord('w'):
@@ -86,17 +108,17 @@ def step_sim(sim, key_code, sim_x, sim_y, sim_z, sim_a, sim_quat, step_size=STEP
     elif key_code == ord('r'):
         obs = sim.reset()
     elif key_code == ord('s'): # BACKWARD
-        x = sim_x + step_size * numpy.sin(sim_a)
-        z = sim_z + step_size * numpy.cos(sim_a)
-        obs = sim.get_observations_at(numpy.array([x, sim_y, z]), sim_quat, True)
+        x = sim_pos[0] + step_size * numpy.sin(sim_a)
+        z = sim_pos[2] + step_size * numpy.cos(sim_a)
+        obs = sim.get_observations_at(numpy.array([x, sim_pos[1], z]), sim_quat, True)
     elif key_code == ord('a'): # STRAFE_LEFT
-        x = sim_x - step_size * numpy.cos(sim_a)
-        z = sim_z + step_size * numpy.sin(sim_a)
-        obs = sim.get_observations_at(numpy.array([x, sim_y, z]), sim_quat, True)
+        x = sim_pos[0] - step_size * numpy.cos(sim_a)
+        z = sim_pos[2] + step_size * numpy.sin(sim_a)
+        obs = sim.get_observations_at(numpy.array([x, sim_pos[1], z]), sim_quat, True)
     elif key_code == ord('d'): # STRAFE_RIGHT
-        x = sim_x + step_size * numpy.cos(sim_a)
-        z = sim_z - step_size * numpy.sin(sim_a)
-        obs = sim.get_observations_at(numpy.array([x, sim_y, z]), sim_quat, True)
+        x = sim_pos[0] + step_size * numpy.cos(sim_a)
+        z = sim_pos[2] - step_size * numpy.sin(sim_a)
+        obs = sim.get_observations_at(numpy.array([x, sim_pos[1], z]), sim_quat, True)
     elif key_code == ord('x'):
         reject = True
     elif key_code == ord('c'):
@@ -104,47 +126,96 @@ def step_sim(sim, key_code, sim_x, sim_y, sim_z, sim_a, sim_quat, step_size=STEP
     return obs, reject
 
 
+def save_obs(output_dir, sim_obs, sim_pos, sim_a):
+    a_deg = numpy.degrees(sim_a)
+    filename = "x{:.3f}_y{:.3f}_z{:.3f}_r{:.0f}_rgb.jpeg".format(*sim_pos, a_deg)
+    filepath = os.path.join(output_dir, filename)
+    cv2.imwrite(filepath, sim_obs["rgb"][:, :, ::-1])
+    depth = (sim_obs["depth"] * 255).astype(numpy.uint8)
+    cv2.imwrite(os.path.join(output_dir, filename.replace("rgb", "depth")), depth)
+    return filepath
+
+
+def calc_offset(sim_pos, sim_a, real_pos, real_a):
+    a = sim_a - real_a
+    if a > numpy.pi:
+        a -= 2*numpy.pi
+    if a < -numpy.pi:
+        a += 2*numpy.pi
+    x = sim_pos[0] + real_pos[0] * numpy.sin(a) + real_pos[1] * numpy.cos(a)
+    y = sim_pos[1]
+    z = sim_pos[2] + real_pos[0] * numpy.cos(a) - real_pos[1] * numpy.sin(a)
+    return [x, y, z], a
+
+
+def apply_offset(sim, off_pos, off_a, real_pos, real_a):
+    x = off_pos[0] - real_pos[0] * numpy.sin(off_a) - real_pos[1] * numpy.cos(off_a)
+    y = off_pos[1]
+    z = off_pos[2] - real_pos[0] * numpy.cos(off_a) + real_pos[1] * numpy.sin(off_a)
+
+    a = off_a + real_a
+    if a > numpy.pi:
+        a -= 2*numpy.pi
+    if a < -numpy.pi:
+        a += 2*numpy.pi
+
+    pos = numpy.array([x, y, z])
+    rot = quaternion.quaternion(numpy.cos(0.5 * a), 0, numpy.sin(0.5 * a), 0)
+    return sim.get_observations_at(pos, rot, True)
+
+
 def main(args):
+    os.makedirs(args.output_dir, exist_ok=True)
+    match_file = open(os.path.join(args.output_dir, "match.csv"), 'w')
+    match_file.write("# REAL RGB IMG PATH,    # SIM RGB IMG PATH\n")
+
     sim = setup_sim(args.step_size, args.turn_angle, args.tilt_angle)
-    obs = sim.reset()
+    sim_obs = sim.reset()
 
     print(USAGE)
 
     measure_cnt = 0
-    offsets = []
-    for (real_x, real_y, real_z, real_a), real_obs in stamped_images(args.input_dir):
-        cv2.imshow("Real robot", real_obs)
-        print_pos("Real robot:", real_x, real_y, real_z, real_a)
+    mean_offset_pos = [0, 0, 0]
+    mean_offset_a = 0
+    for real_pos, real_a, real_obs, real_filepath in stamped_images(args.input_dir):
+        cv2.imshow("Real robot", real_obs if args.no_grid else draw_grid(real_obs))
+        print_pos("Real robot", real_pos, real_a)
+
+        if measure_cnt >= args.n_measures:
+            sim_obs = apply_offset(sim, mean_offset_pos, mean_offset_a, real_pos, real_a)
+
         while True:
             state = sim.get_agent_state()
-            sim_x, sim_y, sim_z = state.position
             alpha, sim_a, gamma = quaternion.as_euler_angles(state.rotation)
             if abs(alpha) > 1.5 and abs(gamma) > 1.5:
                 sim_a *= -1
-            print_pos("Simulation", sim_x, sim_y, sim_z, sim_a, True)
-
-            cv2.imshow("Simulation", obs["rgb"][:, :, ::-1])
+            print_pos("Simulation", state.position, sim_a, True)
+            rgb = sim_obs["rgb"][:, :, ::-1]
+            cv2.imshow("Simulation", rgb if args.no_grid else draw_grid(rgb))
             key_code = cv2.waitKey()
-            nxt_obs, reject = step_sim(sim, key_code, sim_x, sim_y, sim_z, sim_a,
+            nxt_obs, reject = step_sim(sim, key_code, state.position, sim_a,
                                        state.rotation, args.step_size)
             if nxt_obs is None:
                 break
             else:
-                obs = nxt_obs
+                sim_obs = nxt_obs
+
         if reject:
             continue
-        off_a = sim_a - real_a
-        off_x = sim_x + real_x * numpy.sin(off_a) + real_y * numpy.cos(off_a)
-        off_y = sim_y
-        off_z = sim_z + real_x * numpy.cos(off_a) - real_y * numpy.sin(off_a)
-        offsets.append([off_x, off_y, off_z, off_a])
-        print_pos("Offset", off_x, off_y, off_z, off_a)
 
+        off_pos, off_a = calc_offset(state.position, sim_a, real_pos, real_a)
+        print_pos("Offset", off_pos, off_a)
+
+        mean_offset_pos = [(measure_cnt * mu + o) / (measure_cnt + 1)
+                           for mu, o in zip(mean_offset_pos, off_pos)]
+        mean_offset_a = (measure_cnt * mean_offset_a + off_a) / (measure_cnt + 1)
         measure_cnt += 1
-        if measure_cnt == args.n_measures:
-            break
+
+        sim_filepath = save_obs(args.output_dir, sim_obs, state.position, sim_a)
+        match_file.write(real_filepath + ",    " + sim_filepath + "\n")
 
     cv2.destroyAllWindows()
+    match_file.close()
 
 
 if __name__ == "__main__":
