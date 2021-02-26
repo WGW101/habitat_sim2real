82fc37ade41afda87881b7446a6c531c8bbdd7b7
Guillaume Bono <guillaume.bono@insa-lyon.fr>
2021-02-26 15:56:32 +0100

    Added script to retrieve matterport3d datasets from habitat; missing script to download the scenes

---
+++ script/download_mp3d.sh
@@ -0,0 +1,10 @@
+python3 script/download_mp3d_scenes.py --task "habitat" -o "data/scene_datasets/mp3d"
+
+curl "https://dl.fbaipublicfiles.com/habitat/data/datasets/pointnav/mp3d/v1/pointnav_mp3d_v1.zip" -o "pointnav_mp3d_v1.zip" \
+	&& unzip "pointnav_mp3d_v1.zip" -d "data/datasets/pointnav/mp3d/v1"
+curl "https://dl.fbaipublicfiles.com/habitat/data/datasets/objectnav/m3d/v1/objectnav_mp3d_v1.zip" -o "objectnav_mp3d_v1.zip" \
+	&& unzip "objectnav_mp3d_v1" -d "data/datasets/objectnav/mp3d/v1"
+curl "https://dl.fbaipublicfiles.com/habitat/data/datasets/eqa/mp3d/v1/eqa_mp3d_v1.zip" -o "eqa_mp3d_v1.zip" \
+	&& unzip "eqa_mp3d_v1.zip" -d "data/datasets/eqa/mp3d/v1"
+curl "https://dl.fbaipublicfiles.com/habitat/data/datasets/vln/mp3d/r2r/v1/vln_r2r_mp3d_v1.zip" -o "vln_r2r_mp3d_v1.zip" \
+	&& unzip "vln_r2r_mp3d_v1.zip" -d "data/datasets/vln/mp3d/r2r/v1"
