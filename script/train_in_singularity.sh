416965c013d4b7ad2327d48eadcb37f245175b2a
Guillaume Bono <guillaume.bono@insa-lyon.fr>
2021-03-01 12:23:14 +0100

    Changed out dir to rw out folder (instead of ro data linking to /storage)

--- script/train_in_singularity.sh
+++ script/train_in_singularity.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-singularity exec --nv /home/gbono/container_images/habitat_v016_cuda.sif \
+singularity exec -B /storage:/storage:ro --nv /home/gbono/container_images/habitat_v016_cuda.sif \
 	python3 script/run_baseline.py -c "configs/locobot_ppo_citi_sim.yaml" -r "train"
