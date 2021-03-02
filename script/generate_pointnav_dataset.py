21cd7c8cf425afccb710cc2be177d8bd093f9587
Guillaume Bono <guillaume.bono@insa-lyon.fr>
2021-03-02 10:28:52 +0100

    Slight update to dataset generaton script; WIP on multigoal

--- script/generate_pointnav_dataset.py
+++ script/generate_pointnav_dataset.py
@@ -15,7 +15,7 @@ import habitat
 from habitat.tasks.nav.nav import NavigationGoal, NavigationEpisode
 
 
-CFG_PATH = "configs/locobot_pointnav_citi_sim_simple.yaml"
+CFG_PATH = "configs/locobot_pointnav_citi_sim.yaml"
 N_EPISODES = 300
 DIFFICULTIES = ("very easy", "easy", "medium", "hard")
 DIFFICULTY_BOUNDS = (1.0, 3.0, 7.0, 13.0, 20.0)
@@ -56,6 +56,16 @@ def sample_point(sim, radius=MIN_ISLAND_RADIUS):
     return pt
 
 
+def make_episode(ep_id, scene_id, src, dst, success_dist, difficulty, geo_dist):
+    a = 2 * math.pi * random.random()
+    q = [0, math.sin(a / 2), 0, math.cos(a / 2)]
+    goal = NavigationGoal(position=dst, radius=success_dist)
+    episode = NavigationEpisode(episode_id=ep_id, scene_id=scene_id,
+                                start_position=src, start_rotation=q, goals=[goal],
+                                info={"difficulty": difficulty,
+                                      "geodesic_distance": geo_dist})
+
+
 def main(args):
     cfg = habitat.get_config(args.config_path, args.extra_cfg)
     out_file = cfg.DATASET.DATA_PATH.format(split=cfg.DATASET.SPLIT)
@@ -64,41 +74,36 @@ def main(args):
     difficulties = parse_difficulty_ratios(args)
 
     sim = habitat.sims.make_sim(cfg.SIMULATOR.TYPE, config=cfg.SIMULATOR)
+    print("Using seed: {}".format(args.seed))
     sim.seed(args.seed)
     dataset = habitat.datasets.make_dataset(cfg.DATASET.TYPE)
 
     n_pts = 2 * args.n_episodes
     nav_pts = [sample_point(sim) for _ in range(n_pts)]
-    pairs = [(i, j) for i in range(n_pts - 1) for j in range(i, n_pts)]
 
+    pairs = [(i, j) for i in range(n_pts - 1) for j in range(i, n_pts)]
     euc_dists = pairwise_distance(nav_pts)
+
     tot_cnt = 0
     with tqdm.tqdm(total=args.n_episodes) as progress:
         for k, (n_ep, min_d, max_d) in difficulties.items():
-            candidates = [(p, d) for p, d in zip(pairs, euc_dists)
-                          if 0.8 * min_d <= d <= 1.2 * max_d]
+            # Pre-filter candidate pairs of (src, dst) by euclidean distance
+            candidates = [(p, euc_d) for p, euc_d in zip(pairs, euc_dists)
+                          if 0.8 * min_d <= euc_d <= 1.2 * max_d]
             cnt = 0
             for (i, j), euc_d in candidates:
                 src = nav_pts[i]
                 dst = nav_pts[j]
                 d = sim.geodesic_distance(src, dst)
                 if min_d <= d <= max_d and d / euc_d >= MIN_DIST_RATIO:
-                    a = 2 * math.pi * random.random()
-                    q = [0, math.sin(a / 2), 0, math.cos(a / 2)]
-                    goal = NavigationGoal(position=dst,
-                                          radius=cfg.TASK.SUCCESS.SUCCESS_DISTANCE)
-                    episode = NavigationEpisode(episode_id=str(tot_cnt),
-                                                scene_id=cfg.SIMULATOR.SCENE,
-                                                start_position=src,
-                                                start_rotation=q,
-                                                goals=[goal],
-                                                info={"difficulty": k,
-                                                      "geodesic_distance": d})
+                    episode = make_episode(str(tot_cnt), cfg.SIMULATOR.SCENE, src, dst,
+                                           cfg.TASK.SUCCESS.SUCCESS_DISTANCE, k, d)
                     dataset.episodes.append(episode)
                     cnt += 1
                     tot_cnt += 1
                     progress.update(1)
                     if cnt == n_ep:
+                        # Skip to next difficulty
                         break
 
     with gzip.open(out_file, "wt") as f:
