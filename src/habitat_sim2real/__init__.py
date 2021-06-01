1efa16a661ce0d29675ff4a834aa20a17232c6fc
Guillaume Bono <guillaume.bono@insa-lyon.fr>
2021-06-01 13:32:55 +0200

    Updated import error filter to store exceptions in global vars...

--- src/habitat_sim2real/__init__.py
+++ src/habitat_sim2real/__init__.py
@@ -4,16 +4,18 @@ from .utils.perlin import perlin_1d, perlin_2d
 try:
     from .sims.pyrobot.pyrobot_discrete_action import PyRobotDiscreteActions
 except ImportError as e:
+    pyrobot_import_error = e
     class PyRobotDiscreteActions:
         def __init__(*args, **kwargs):
-            raise e
+            raise pyrobot_import_error
 
 try:
     from .sims.habitat_simulator.realistic_depth_sensor import RealisticHabitatSimDepthSensor
 except ImportError as e:
+    habitat_sim_import_error = e
     class RealisticHabitatSimDepthSensor:
         def __init__(*args, **kwargs):
-            raise e
+            raise habitat_sim_import_error
 
 try:
     from .sims.ros.rosrobot_sim import ROSRobot
@@ -21,26 +23,28 @@ try:
     from .sims.ros.default_cfg import merge_ros_config, get_config
     from .envs.ros_env import ROSEnv, ROSNavRLEnv
 except ImportError as e:
+    ros_import_error = e
     class ROSRobot:
         def __init__(*args, **kwargs):
-            raise e
+            raise ros_import_error
     class HabitatInterfaceROSNode:
         def __init__(*args, **kwargs):
-            raise e
+            raise ros_import_error
     def merge_ros_config(*args, **kwargs):
-        raise e
+        raise ros_import_error
     def get_config(*args, **kwargs):
-        raise e
+        raise ros_import_error
     class ROSEnv:
         def __init__(*args, **kwargs):
-            raise e
+            raise ros_import_error
     class ROSNavRLEnv:
         def __init__(*args, **kwargs):
-            raise e
+            raise ros_import_error
 
 try:
     from .sims.jetbot.hetbot_sim import Jetbot
 except ImportError as e:
+    jetbot_import_error = e
     class Jetbot:
         def __init__(*args, **kwargs):
-            raise e
+            raise jetbot_import_error
