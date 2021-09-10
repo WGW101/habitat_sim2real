7b6f678460d81775222fda1b1cc3e00da73f2367
Guillaume Bono <guillaume.bono@insa-lyon.fr>
2021-09-10 10:33:10 +0200

    Refactored parallel_sim to use separated processes for master and slave simulators, enabling 2 habitatsim to run in parallel (opengl context limitation)

--- src/habitat_sim2real/sims/parallel_sim/parallel_sim.py
+++ src/habitat_sim2real/sims/parallel_sim/parallel_sim.py
@@ -1,12 +1,114 @@
-from typing import Union, Any, Optional, Dict
+from typing import Union, Any, Optional, Dict, Tuple
+import multiprocessing as mp
+import enum
+import logging
+import os
 
-from gym.spaces import Space
 import numpy as np
 import quaternion
 
 from habitat.core.simulator import Simulator, Observations, AgentState
 from habitat.config.default import Config
 from habitat.sims import make_sim
+
+
+_logger = logging.getLogger(__name__)
+_logger.addHandler(logging.StreamHandler())
+_logger.setLevel(logging.DEBUG)
+
+
+class SlaveProcess(mp.Process):
+    @staticmethod
+    def state_to_tuple(s: AgentState) -> Tuple[float]:
+        return (*s.position.tolist(), s.rotation.w, s.rotation.x, s.rotation.y, s.rotation.z)
+
+    @staticmethod
+    def tuple_to_pos_rot(tup: Tuple[float]) -> Tuple[np.array, np.quaternion]:
+        return np.array(tup[:3]), np.quaternion(*tup[3:])
+
+    @staticmethod
+    def array_to_shared(src: np.ndarray, dest: mp.Array, typ: str) -> None:
+        wrap = np.frombuffer(dest.get_obj(), dtype=typ)
+        with dest:
+            wrap[:] = src.flatten()
+
+    class Messages(enum.Enum):
+        READY = enum.auto()
+        SEED = enum.auto()
+        RECONFIGURE = enum.auto()
+        GET_STATE = enum.auto()
+        GET_COLLIDED = enum.auto()
+        GET_OBS_AT = enum.auto()
+        RESET = enum.auto()
+        STEP = enum.auto()
+        RETURN_NONE = enum.auto()
+        RETURN_STATE = enum.auto()
+        RETURN_COLLIDED = enum.auto()
+        RETURN_OBS = enum.auto()
+        TERMINATE = enum.auto()
+
+    _config: Config
+    _conn: mp.connection.Connection
+    shared_rgb: mp.Array
+    shared_depth: mp.Array
+
+    def __init__(self, config: Config) -> None:
+        super().__init__()
+        self._config = config
+        self._conn = None
+        self.shared_rgb = mp.Array('B', config.RGB_SENSOR.WIDTH * config.RGB_SENSOR.HEIGHT * 3)
+        self.shared_depth = mp.Array('f', config.DEPTH_SENSOR.WIDTH * config.DEPTH_SENSOR.HEIGHT)
+        _logger.debug("[SLAVE] Initialized slave proc.")
+
+    def connect(self) -> mp.connection.Connection:
+        parent_conn, self._conn = mp.Pipe()
+        return parent_conn
+
+    def run(self) -> None:
+        _logger.debug("[SLAVE] Running slave proc.")
+        _logger.debug(f"[SLAVE] {os.getpid()=}")
+        with make_sim(self._config.TYPE, config=self._config) as sim:
+            _logger.debug(f"[SLAVE] Created slave sim")
+            self._conn.send((SlaveProcess.Messages.READY, None))
+            while True:
+                _logger.debug(f"[SLAVE] Waiting for msg")
+                msg, arg = self._conn.recv()
+                if msg == SlaveProcess.Messages.RESET:
+                    sim.reset()
+                    self._conn.send((SlaveProcess.Messages.RETURN_NONE, None))
+                elif msg == SlaveProcess.Messages.SEED:
+                    sim.seed(arg)
+                    self._conn.send((SlaveProcess.Messages.RETURN_NONE, None))
+                elif msg == SlaveProcess.Messages.RECONFIGURE:
+                    sim.reconfigure(arg)
+                    self._conn.send((SlaveProcess.Messages.RETURN_NONE, None))
+                elif msg == SlaveProcess.Messages.GET_STATE:
+                    s = sim.get_agent_state()
+                    self._conn.send((SlaveProcess.Messages.RETURN_STATE,
+                                     SlaveProcess.state_to_tuple(s)))
+                elif msg == SlaveProcess.Messages.GET_COLLIDED:
+                    self._conn.send((SlaveProcess.Messages.RETURN_COLLIDED,
+                                     sim.previous_step_collided))
+                elif msg == SlaveProcess.Messages.GET_OBS_AT:
+                    pos, rot = SlaveProcess.tuple_to_pos_rot(arg)
+                    obs = sim.get_observations_at(pos, rot, True)
+                    SlaveProcess.array_to_shared(obs['rgb'], self.shared_rgb, 'B')
+                    SlaveProcess.array_to_shared(obs['depth'], self.shared_depth, 'f')
+                    self._conn.send((SlaveProcess.Messages.RETURN_OBS, None))
+                elif msg == SlaveProcess.Messages.STEP:
+                    sim.step(arg)
+                    self._conn.send((SlaveProcess.Messages.RETURN_NONE, None))
+                elif msg == SlaveProcess.Messages.TERMINATE:
+                    break
+
+    def get_shared_obs(self) -> Dict[str, np.ndarray]:
+        wrap = np.frombuffer(self.shared_rgb.get_obj(), dtype='B')
+        with self.shared_rgb:
+            cp_rgb = wrap.copy()
+        wrap = np.frombuffer(self.shared_depth.get_obj(), dtype='f')
+        with self.shared_depth:
+            cp_depth = wrap.copy()
+        return {"rgb": cp_rgb, "depth": cp_depth}
 
 
 def make_parallel(master_cls, *, slave_type: Optional[str]=None,
@@ -20,30 +122,49 @@ def make_parallel(master_cls, *, slave_type: Optional[str]=None,
 
 
     class ParallelSimulator(master_cls):
-        _slave_sim: Simulator
+        _slave_proc: SlaveProcess
+        _conn: mp.connection.Connection
         _state_diff: AgentState
         _collision_diff: bool
         _slave_obs: Optional[Observations]
 
         def __init__(self, master_config: Config, *args: Any, **kwargs: Any) -> None:
             super().__init__(master_config)
+            _logger.debug("[MASTER] Initialized super() on master proc.")
+            _logger.debug(f"[MASTER] {os.getpid()=}")
             slave_cfg = master_config.clone()
             slave_cfg.merge_from_other_cfg(slave_config)
-            self._slave_sim = make_sim(slave_cfg.TYPE, config=slave_cfg)
+            mp.set_start_method("forkserver")
+            self._slave_proc = SlaveProcess(slave_cfg)
+            self._conn = self._slave_proc.connect()
+            self._slave_proc.start()
+
+            msg, arg = self._conn.recv()
+            assert msg == SlaveProcess.Messages.READY
+
             self._state_diff = AgentState(None, None)
             self._collision_diff = 0
             self._slave_obs = None
+            _logger.debug("[MASTER] Initialized master proc.")
 
         def _update_slave_state_obs(self) -> None:
-            master_s = self.get_agent_state()
-            slave_s = self._slave_sim.get_agent_state()
-            self._state_diff.position = slave_s.position - master_s.position
-            self._state_diff.rotation = slave_s.rotation * master_s.rotation.conjugate()
-            self.collision_diff = (self._slave_sim.previous_step_collided
-                                   != self.previous_step_collided)
-            self.slave_obs = self._slave_sim.get_observations_at(master_s.position,
-                                                                 master_s.rotation,
-                                                                 True)
+            s = self.get_agent_state()
+
+            self._conn.send((SlaveProcess.Messages.GET_STATE, None))
+            msg, arg = self._conn.recv()
+            assert msg == SlaveProcess.Messages.RETURN_STATE
+            self._state_diff.position = np.array(arg[:3]) - s.position
+            self._state_diff.rotation = np.quaternion(*arg[3:]) * s.rotation.conjugate()
+
+            self._conn.send((SlaveProcess.Messages.GET_COLLIDED, None))
+            msg, arg = self._conn.recv()
+            assert msg == SlaveProcess.Messages.RETURN_COLLIDED
+            self.collision_diff = (arg != self.previous_step_collided)
+
+            self._conn.send((SlaveProcess.Messages.GET_OBS_AT, SlaveProcess.state_to_tuple(s)))
+            msg, arg = self._conn.recv()
+            assert msg == SlaveProcess.Messages.RETURN_OBS
+            self._slave_obs = self._slave_proc.get_shared_obs()
 
         def get_last_state_diff(self) -> AgentState:
             return self._state_diff
@@ -56,24 +177,43 @@ def make_parallel(master_cls, *, slave_type: Optional[str]=None,
 
         def reset(self) -> Observations:
             obs = super().reset()
-            self._slave_sim.reset()
+
+            self._conn.send((SlaveProcess.Messages.RESET, None))
+            msg, arg = self._conn.recv()
+            assert msg == SlaveProcess.Messages.RETURN_NONE
+
             self._update_slave_state_obs()
             return obs
 
         def step(self, action: Union[str, int], *args: Any, **kwargs: Any) -> Observations:
             obs = super().step(action, *args, **kwargs)
-            self._slave_sim.step(action, *args, **kwargs)
+
+            self._conn.send((SlaveProcess.Messages.STEP, action))
+            msg, arg = self._conn.recv()
+            assert msg == SlaveProcess.Messages.RETURN_NONE
+
             self._update_slave_state_obs()
             return obs
 
         def seed(self, seed: int) -> None:
             super().seed(seed)
-            self._slave_sim.seed(seed)
+
+            self._conn.send((SlaveProcess.Messages.SEED, seed))
+            msg, arg = self._conn.recv()
+            assert msg == SlaveProcess.Messages.RETURN_NONE
 
         def reconfigure(self, config: Config) -> None:
             super().reconfigure(config)
             slave_cfg = config.clone()
             slave_cfg.merge_from_other_cfg(slave_config)
-            self._slave_sim.reconfigure(slave_cfg)
+
+            self._conn.send((SlaveProcess.Messages.RECONFIGURE, slave_cfg))
+            msg, arg = self._conn.recv()
+            assert msg == SlaveProcess.Messages.RETURN_NONE
+
+        def close(self) -> None:
+            self._conn.send((SlaveProcess.Messages.TERMINATE, None))
+            self._slave_proc.join()
+            super().close()
 
     return ParallelSimulator
